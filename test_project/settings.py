857a8e127a66793c906a141745e36c2396525c5a
kamral <kamral010101@gmail.com>
2021-01-20 19:10:00 +0300

    fix it

--- test_project/settings.py
+++ test_project/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIR=[
-    os.path.join(BASE_DIR,'test_project')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'test_project/static')
 ]
 
 MEDIA_URL='/media/'
